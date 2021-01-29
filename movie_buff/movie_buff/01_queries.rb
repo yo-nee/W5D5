34d92f702b7dbe7bea2bc88dad0345c585556cd7
alejandro <alejandroweil13@gmail.com>
2021-01-29 15:26:00 -0500

    Finished directed_by_one_of

--- movie_buff/movie_buff/01_queries.rb
+++ movie_buff/movie_buff/01_queries.rb
@@ -64,6 +64,11 @@ def directed_by_one_of(them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
 
+  Movie
+  .select(:id, :title)
+  .joins(:director)
+  .where(actors: {name: them})
+
 end
 
 def movie_names_before_1940
