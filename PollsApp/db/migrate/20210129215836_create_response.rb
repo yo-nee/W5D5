0313198155fec3671970a44d4a2168f6199b8241
yonatan zonszein <yonatanzonszein@gmail.com>
2021-01-29 21:05:27 -0500

    Began polls

--- PollsApp/db/migrate/20210129215836_create_response.rb
+++ PollsApp/db/migrate/20210129215836_create_response.rb
@@ -1,7 +1,8 @@
 class CreateResponse < ActiveRecord::Migration[6.1]
   def change
     create_table :responses do |t|
-      t.
+      t.integer :question_id, null: false
+      t.integer :answer_choice_id, null: false
       t.timestamps
     end
   end
